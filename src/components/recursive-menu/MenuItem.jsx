617f84566c476d484955a295b36eda9d2bad66de
priyanshu <oppriyanshu@gmail.com>
2024-11-04 22:47:38 +0530

    recursive menu updated

--- src/components/recursive-menu/MenuItem.jsx
+++ src/components/recursive-menu/MenuItem.jsx
@@ -1,10 +1,30 @@
-export default function MenuItem({item}){
+import MenuList from "./MenuList";
 
-    return (
+export default function MenuItem({ item }) {
+
+  const [displayCurrentChildren , setDisplayCurrentChildren] = useState({})
+
+  function handleToggle(){
+
+
+  }
+  return (
+    <li>
+    <div style={{
+      display:"flex",
+      gap:'20px'
+    }}>
+    <ul>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length>0 ? <span>+</span>:null}
+      </ul>
+    </div>
+
+      {item && item.children && item.children.length > 0 ? (
         <div>
-           hello
+          <MenuList list={item.children} />
         </div>
-    )
-
-
+      ) : null}
+    </li>
+  );
 }
--- src/components/recursive-menu/MenuList.jsx
+++ src/components/recursive-menu/MenuList.jsx
@@ -1,21 +1,15 @@
-import MenuItem from "./MenuItem"
+import MenuItem from "./MenuItem";
 
-export default function MenuList({list =[]}){
-    
-   return(
-    <div>
-         {list && list.length >0? (
-            <div>
-               { list.map((item,index)=>(
-              
-
-                    <MenuItem key={index} item={item} />  
-          
-            )
-
-                )}
-            </div>
-         ):null}
-    </div>
-   )
+export default function MenuList({ list = [] }) {
+  return (
+    <ul className="menu-list-container">
+      {list && list.length > 0 ? (
+        <div>
+          {list.map((item, index) => (
+            <MenuItem key={index} item={item} />
+          ))}
+        </div>
+      ) : null}
+    </ul>
+  );
 }
